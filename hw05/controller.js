6eb149b4e805dcb33afb882aa541a2b899ecad70
luchick1994 <luchick94@mail.ru>
2014-09-02 19:27:57 +0300

    Initial

--- hw05/controller.js
+++ hw05/controller.js
@@ -1,18 +1,18 @@
-var shopcatApp = angular.module('shopcatApp', []);
+var shopApp = angular.module('shopApp', []);
 
-shopcatApp.controller('shopListCtrl', function ($scope) {
+shopApp.controller('shopCtrl', function ($scope) {
   $scope.shops = [
     {'name': '15.6" [Home] Ноутбук DNS (0164783) (HD)',
      'price': '20 490 руб.',
- 	  'imgurl': 'src/img/1.jpg',
+ 	  'imgurl': 'img/1.jpg',
  		'incard': 0},
     {'name': '17.3" [Gamer] Ноутбук DNS (0801153) (HD+)',
      'price': '33 990 руб.',
- 	  'imgurl': 'src/img/2.jpg',
+ 	  'imgurl': 'img/2.jpg',
  	  'incard': 0},
  	  {'name': 'Компьютер DNS Prestige XL [0800795]',
      'price': '59 990 руб.',
- 	  'imgurl': 'src/img/3.jpg',
+ 	  'imgurl': 'img/3.jpg',
  	  'incard': 0},
   ];
 
